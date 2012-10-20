d9f165148b6329c5d0aaa7dcf20dc72fa7a866ad
Oleksandr Kharchenko <okharch@gmail.com>
2012-10-20 08:10:40 +0300

    refactoring crypto methods

--- lib/SkyClip/Interface.pm
+++ lib/SkyClip/Interface.pm
@@ -9,23 +9,33 @@ use XML::Simple;
 use base 'Exporter';
 our @EXPORT=qw(request response header);
 use const header => "Content-type: application/xml";
+use HTTP::Request;
 
 sub get_post_data { # data is in request body. can be binary like compressed
     my $q = $CGI->new;
     my ($user,$sign) = map {$q->param($_))||return "Invalid request: param $_ not found"} qw(user sign);
-    $user =~ s/[^a-z0-9]//g; # remove dangerous symbols from user param
-    my $user_folder = USER_DIR . "/$user";
-    my $user_public_key = check_sign($data,$sign,"$user_folder/public.key";
-    $user_public_key = read_file($user_public_key) || return "User public key not found";
-    my $user_rsa = $rsa->new_public_key($user_public_key) || return "Invalid user public key";
+    my ($user_folder) = user_folder $user || return 'Invalid user';
+    my ($error,$user_public_key) = read_key($user_folder,'public');
+    return $error if $error;
     my $data = $q->param('POSTDATA') || return 'Invalid request: clip body not found';
-    return "Invalid clip data/user signature" unless $rsa->verify($data, $sign);
-    return {user_folder=> $user_folder,user_data => $user_data};    
+    return "Invalid clip data/user signature" unless $user_public_key->verify($data, $sign);
+    return ($user_folder,$user_data);    
 }
 
+=item put_request_data
+This sub makes post request consistent with that which get_post_data expects
+i.e. it compresses data & crypt it with user public key so only priv key can decrypt this
+then it signs it with user priv key in order server can check signature
+=cut
+
 sub put_request_data {
-    my $data = shift;
-    my $user = $
+    my ($user,$data) = @_;
+    my ($error,$user_priv) = read_key(client_path,"private");
+    return $error if $error;
+    my ($error,$user_public) = read_key(client_path,"public");
+    return $error if $error; 
     $data = compress($data);
-    
+    $data = $user_public->encrypt($data);
+    my $sign = $user_priv->sign($data);
+    return (0,HTTP::Request->new("$CLIP_SERVER?user=$user&sign=$sign",,$data));
 }
--- lib/SkyClip/RSA.pm
+++ lib/SkyClip/RSA.pm
@@ -5,9 +5,9 @@ use SkyClip::Const;
 use Crypt::OpenSSL::RSA;
 use File::Slurp qw(read_file);
 use base 'Exporter';
-our @EXPORT = qw(check_sign server_public_key user_public_key);
+our @EXPORT = qw($rsa check_sign server_public_key user_public_key read_key);
 
-my $rsa = "Crypt::OpenSSL::RSA";
+our $rsa = "Crypt::OpenSSL::RSA";
 sub check_sign { my ($data,$sign,$public_key) = @_;
     my $cs = $rsa->new_public_key($public_key) || error("Invalid public key");
     $rsa->verify($data, $sign) || error("Invalid data/sign");
@@ -20,5 +20,14 @@ sub server_public_key {
 sub user_public_key { my $user = shift;
     read_file(user_folder($user)."/public.key") || error("Can't read user public key");
 }
+
+sub read_key { my ($folder,$key) = @_; # return $error,$key
+    my $file = "$folder/$key.key";
+    my ($user_key) = grep(-f,$file) || return "User $key key not found";
+    $user_key = read_file($user_key) || return "Can't read user $key key";
+    my $method = "new_${key}_key";
+    return (0,$rsa->$method($user_key)) || return "Invalid user $key key";    
+}
+
 
 1;
